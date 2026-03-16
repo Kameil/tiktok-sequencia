2e86801b5dcc7eb5c54874938c132f26af13bdce
bbpltergiest <83366426+bbpltergiest@users.noreply.github.com>
2026-03-16 17:09:35 -0300

    this does not work btw

--- main.py
+++ main.py
@@ -8,17 +8,19 @@ from platform import system
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-import keyboard
+from notifypy import Notify
+from pynput import keyboard
 
 from config import get_paths_by_os, get_flags_by_os
 
 today = datetime.now()
-with open("hoje.txt", "r", encoding="utf-8") as file:
+with open("t.txt", "r", encoding="utf-8") as file:
     if file.read() == str(today.day):
         print("Already sent today")
         time.sleep(5)
         exit()
 
+should_exit = False
 option = Options()
 chrome_paths = get_paths_by_os()
 chrome_path = None
@@ -48,67 +50,70 @@ browser = webdriver.Chrome(options=option)
 
 browser.get("https://tiktok.com/")
 
-# # ENTRANDO NA CONTA
+if not os.path.exists("cookies.pkl"):
+    print("No account found. Please log in and press Ctrl+S to save your cookies.")
+    ntf = Notify()
+    ntf.title = "Login with your account"
+    ntf.message = "It Was not possible to find any account. Please log in and save your cookies."
+    ntf.send()
+    time.sleep(1)
 
-# if not os.path.exists("cookies.pkl"):
-#     print(
-#         "Nenhuma conta encontrada, entre na sua e aperte ctrl+s para salvar os cookies"
-#     )
-#     # ntf = Notification(
-#     #     app_id="codigo",
-#     #     title="Logue com sua conta",
-#     #     msg="Nenhuma conta encontrada, entre na sua e aperte ctrl+s para salvar os cookies",
-#     # )
-#     # ntf.show()
-#     time.sleep(1)
+    def save_cookies():
+        pickle.dump(browser.get_cookies(), open("cookies.pkl", "wb"))
+        print("Saved cookies.")
+        ntf = Notify()
+        ntf.title = "Cookies saved"
+        ntf.message = "Please make sure you are logged in."
+        ntf.send()
 
-#     def salvar_cookies():
-#         pickle.dump(browser.get_cookies(), open("cookies.pkl", "wb"))
-#         print("cookies salvos")
-#         # ntf = Notification(
-#         #     app_id="codigo",
-#         #     title="cookies salvos esc para sair ou ctrl+s para salvar novamente",
-#         #     msg="certifique-se de ter entrado na conta.",
-#         # )
-#         # ntf.show()
+    def on_press(key):
+        if key == keyboard.Key.esc:
+            return False
 
-#     keyboard.add_hotkey("ctrl+s", lambda: salvar_cookies())
-#     print("aperte esc para sair ou ctrl+s para salvar denovo.")
-#     keyboard.wait("esc")
-#     browser.quit()
-#     time.sleep(1)
-#     exit()
+        if key == keyboard.KeyCode.from_char('s') and any(
+                modifier in [keyboard.Key.ctrl_l, keyboard.Key.ctrl_r]
+                for modifier in [keyboard.Key.ctrl_l, keyboard.Key.ctrl_r]):
+            save_cookies()
 
-# # JA ENTRADO
-# cookies = pickle.load(open("cookies.pkl", "rb"))
-# for cookie in cookies:
-#     browser.add_cookie(cookie)
-# browser.refresh()
-# time.sleep(1)
-# # ir para mensagens
-# browser.get("https://www.tiktok.com/messages?lang=pt-BR")
-# time.sleep(10)
-# # clicar no primeiro usuario
-# browser.find_element(By.XPATH, '//*[@id="more-acton-icon-0"]/div/div[1]/div').click()
-# time.sleep(2)
-# # clicar na caixa de texto
-# browser.find_element(By.CLASS_NAME, "DraftEditor-root").click()
-# time.sleep(1)
+    print("Press Esc to exit or Ctrl+S to save again.")
 
-# # enviar
-# palavras = ["desenrolado", "orea seca"]
-# for x in random.choice(palavras):
-#     x = str(x)
-#     x = x.replace(" ", "space")
-#     keyboard.press(x)
-#     time.sleep(0.2)
-# time.sleep(0.5)
-# keyboard.press("enter")
-# with open("hoje.txt", "w", encoding="utf-8") as file:
-#     file.write(str(today.day))
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
 
-# # ntf = Notification(app_id="tiktok sequencia", title="Sequencia enviada")
-# # ntf.show()
+    browser.quit()
+    time.sleep(1)
+    exit()
 
-# time.sleep(2)
-# browser.quit()
+# JA ENTRADO
+cookies = pickle.load(open("cookies.pkl", "rb"))
+for cookie in cookies:
+    browser.add_cookie(cookie)
+browser.refresh()
+time.sleep(1)
+# ir para mensagens
+browser.get("https://www.tiktok.com/messages?lang=pt-BR")
+time.sleep(10)
+# clicar no primeiro usuario
+browser.find_element(By.XPATH, '//*[@id="more-acton-icon-0"]/div/div[1]/div').click()
+time.sleep(2)
+# clicar na caixa de texto
+browser.find_element(By.CLASS_NAME, "DraftEditor-root").click()
+time.sleep(1)
+
+# enviar
+palavras = ["desenrolado", "orea seca"]
+for x in random.choice(palavras):
+    x = str(x)
+    x = x.replace(" ", "space")
+    keyboard.press(x)
+    time.sleep(0.2)
+time.sleep(0.5)
+keyboard.press("enter")
+with open("t.txt", "w", encoding="utf-8") as file:
+     file.write(str(today.day))
+
+# ntf = Notification(app_id="tiktok sequencia", title="Sequencia enviada")
+# ntf.show()
+
+time.sleep(2)
+browser.quit()
